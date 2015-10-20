f971e7d49a69e0799a1ecdb5a7aac5c0ed86cce8
Arthur Lemaitre <arthur@mavenlink.com>
2015-10-19 23:02:50 -0700

    setup meta title and description but they're not rendering yet

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,4 +1,6 @@
 class BaseController < ApplicationController
   def home
+    @meta_title = "Mavenlink Design Challenge"
+    @meta_description = "Arthur design challenge."
   end
 end
--- app/controllers/industries_controller.rb
+++ app/controllers/industries_controller.rb
@@ -1,4 +1,6 @@
 class IndustriesController < ApplicationController
   def dog_groomers
+    @meta_title = "Dog Grooming with Mavenlink"
+    @meta_description = "Use Mavenlink to maximize your dog grooming business' profitability."
   end
 end
