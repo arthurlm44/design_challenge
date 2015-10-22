0c007a8d6ecc2365e97732f0e6e88f62d190e28d
Arthur Lemaitre <arthur@mavenlink.com>
2015-10-21 17:51:17 -0700

    fixed stupid typo

--- app/controllers/industries_controller.rb
+++ app/controllers/industries_controller.rb
@@ -1,5 +1,5 @@
 class IndustriesController < ApplicationController
-  def dog_groomers
+  def dog_groomer
     @meta_title = "Dog Grooming with Mavenlink"
     @meta_description = "Use Mavenlink to maximize your dog grooming business' profitability."
   end
